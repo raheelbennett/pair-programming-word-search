0cb41f517d9c3ffb33fbf37fdb8e5df72c1f82ff
Raheel Bennett <raheel.bennett@telus.com>
2022-11-23 18:27:30 -0700

    pair programming

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const { wordSearch } = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -35,4 +35,55 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CICJ')
+
+    assert.isTrue(result);
+  });
+  it("should return false if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CICK')
+
+    assert.isFalse(result);
+  });
+  it("should return false if the word matrix is empty", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], ' ')
+
+    assert.isFalse(result);
+  });
+  it("should return false if the letters matrix is empty", function() {
+    const result = wordSearch([], 'CICK')
+
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// Transpose function from previous pair programming activity
+const transpose = function (matrix) {
+  let transformedArray = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    transformedArray.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      transformedArray[i].push(matrix[j][i]);
     }
-}
+  } return transformedArray;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (!letters.length) { //An empty matrix will now return false rather than undefined. 
+    return false
+  };
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true
+  }
+  //We are using code from yesterday's pair programming to transpose our letters array matrix to now have vertical letters run through the loop.
+  const transposedLetters = transpose(letters);
+  const verticalJoin = transposedLetters.map(ls => ls.join(''))
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true
+  }
+  return false;
+};
+
+module.exports = { wordSearch };
+
+
